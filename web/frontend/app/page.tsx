d2356a12ad0f2b7b49182861a17e9e8dd7a129df
Mark Koberlein <mkoberlein@gmail.com>
2024-03-07 22:44:06 -0500

    added frontend

---
+++ web/frontend/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="bg-gradient-to-br from-violet-900 to bg-purple-950 h-dvh flex items-center justify-center">
+      <figure>
+        <img src="senzei-learning-stack-logo-white.svg" />
+      </figure>
+    </main>
+  );
+}
